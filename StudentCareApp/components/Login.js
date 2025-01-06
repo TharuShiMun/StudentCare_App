ffa3a219582a786074226b74e7d880997e8287e4
TharuShiMun <139700921+TharuShiMun@users.noreply.github.com>
2025-01-06 02:38:56 +0000

    Implement layout for login form and add styles

--- StudentCareApp/components/Login.js
+++ StudentCareApp/components/Login.js
@@ -1,13 +1,67 @@
 import {useState} from "react";
-import {View} from 'react-native';
+import {TextInput, View} from 'react-native';
+import {useNavigation} from "@react-navigation/native";
+
 
 export default function Login(){
     const [name ,setName]=useState("");
     const [password ,setPassword]=useState("");
     const navigate = useNavigation();
+
+
+
+    
     return(
-        <>
-        </>
+        <PaperProvider>
+            <View style={styles.container}>
+               <Text variant="headlineLarge" style={styles.header}>Student Login</Text>
+            </View>
+
+            <View>
+                <TextInput 
+                lable="Username" 
+                mode="outlined" 
+                value={name}
+                onChangeText={setName}
+                style={styles.input}/>
+            </View>
+
+            <View>
+                <TextInput
+                lable="Password"
+                mode="outlined"
+                value={password}
+                onchangeText={setPassword}
+                secureTextentry
+                style={styles.input}/>
+            </View>
+
+            <View>
+                <Button 
+                mode="contained"
+                onPress={()=>navigate.navigate('')}></Button>
+            </View>
+        </PaperProvider>
     );
 
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      padding: 20,
+      backgroundColor: "#fff",
+    },
+    header: {
+      textAlign: "center",
+      marginBottom: 20,
+    },
+    input: {
+      marginBottom: 15,
+    },
+    button: {
+      marginTop: 10,
+    },
+  });
+  
