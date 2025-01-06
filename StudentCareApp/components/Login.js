16831d58a1c63bb326c042eecaeb142bcd6bbb08
TharuShiMun <139700921+TharuShiMun@users.noreply.github.com>
2025-01-06 01:33:07 +0000

    feat:define state variables and navigation setup

---
+++ StudentCareApp/components/Login.js
@@ -0,0 +1,13 @@
+import {useState} from "react";
+import {View} from 'react-native';
+
+export default function Login(){
+    const [name ,setName]=useState("");
+    const [password ,setPassword]=useState("");
+    const navigate = useNavigation();
+    return(
+        <>
+        </>
+    );
+
+}
