2944022076ab3a1a3b82d8ff207fe97f3c8f5cdb
TharuShiMun <139700921+TharuShiMun@users.noreply.github.com>
2025-01-06 05:13:18 +0000

    function to handle the login button click

--- StudentCareApp/components/Login.js
+++ StudentCareApp/components/Login.js
@@ -8,6 +8,18 @@ export default function Login() {
   const [password, setPassword] = useState("");
   const navigate = useNavigation();
 
+  const handlebutton = () => {
+    const student = students.find(
+      (student) => student.username === name && student.password === password
+    );
+
+    if (student) {
+      navigate.navigate("profile", { studentdata: student });
+    } else {
+      alert("Invalid username or password.");
+    }
+  };
+
   return (
     <PaperProvider>
       <View style={styles.container}>
